8d52c0914972efaa9f0a45afd8661db271ee9190
Simon <52073517+rodonisi@users.noreply.github.com>
2026-05-12 19:15:17 +0200

    feat: save credentials in secure storage (#126)
    * use secure storage for credentials
    
    * warnings
    
    * version
    
    * libsecret linux dep
    
    * clear old entry from drift
    
    * ios/macos
    
    * match macos
    
    * pin 10.0.0
    
    * bump version
    
    * macos codesign local
    
    * macos ci codesign
    
    * implement deleteOutOfDate
    
    * update background task

---
+++ lib/riverpod/repository/secure_storage.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/experimental/persist.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'secure_storage.freezed.dart';
+part 'secure_storage.g.dart';
+
+@freezed
+sealed class SecureStorageEntry with _$SecureStorageEntry {
+  const factory SecureStorageEntry({
+    required String key,
+    required String value,
+    DateTime? expireAt,
+    String? destroyKey,
+  }) = _SecureStorageEntry;
+
+  factory SecureStorageEntry.fromJson(Map<String, Object?> json) =>
+      _$SecureStorageEntryFromJson(json);
+}
+
+@Riverpod(keepAlive: true)
+Storage<String, String> secureStorage(Ref ref) {
+  return SecureStorageRepository();
+}
+
+final class SecureStorageRepository extends Storage<String, String> {
+  final storage = const FlutterSecureStorage();
+
+  SecureStorageRepository();
+
+  @override
+  FutureOr<void> delete(String key) async {
+    await storage.delete(key: key);
+  }
+
+  @override
+  Future<void> deleteOutOfDate() async {
+    final entries = await storage.readAll();
+    final now = DateTime.timestamp();
+    for (final entry in entries.entries) {
+      final storageEntry = SecureStorageEntry.fromJson(jsonDecode(entry.value));
+      if (storageEntry.expireAt != null &&
+          storageEntry.expireAt!.isBefore(now)) {
+        await storage.delete(key: entry.key);
+      }
+    }
+  }
+
+  @override
+  FutureOr<PersistedData<String>?> read(String key) async {
+    final value = await storage.read(key: key);
+    if (value == null) {
+      return null;
+    }
+
+    final entry = SecureStorageEntry.fromJson(jsonDecode(value));
+
+    return PersistedData(entry.value);
+  }
+
+  @override
+  FutureOr<void> write(String key, String value, StorageOptions options) async {
+    final entry = SecureStorageEntry(
+      key: key,
+      value: value,
+      expireAt: options.cacheTime.duration != null
+          ? DateTime.timestamp().add(options.cacheTime.duration!)
+          : null,
+      destroyKey: options.destroyKey,
+    );
+
+    await storage.write(key: key, value: jsonEncode(entry.toJson()));
+  }
+}
